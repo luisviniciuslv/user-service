6433fcc25682f5165acb58310c7b4706b547d5e3
Viini <camperqueodeiacamper@gmail.com>
2022-07-05 14:24:41 -0300

    chore: config application server

---
+++ src/app.ts
@@ -0,0 +1,20 @@
+import bodyParser from "body-parser";
+import cors from 'cors'
+import express, { Application } from "express";
+
+export class App {
+    public server: Application
+
+    constructor(){
+
+        this.server = express()
+        this.setConfig()
+    }
+    private setConfig(){
+
+        this.server.use(bodyParser.json({limit: '50mb'}))
+        this.server.use(bodyParser.urlencoded({limit: '50mb', extended: true }))
+        this.server.use(cors())
+
+    }
+}
---
+++ src/server.ts
@@ -0,0 +1,3 @@
+import {App} from './app'
+
+new App().server.listen(7777, () => console.log('Listening on port 7777'))
